187f2017c6b51d7ef7affb2ec1f8f60a1f80e76f
louischarbonier <louis.charbonier@y-nov.com>
2015-04-09 12:28:54 +0200

    api

--- classes/testU.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once ("user.php");
-	require_once ("magasin.php");
-	//$test = new User(1,"VerrillDuplessis@jourrapide.com", "Duplessis", "Verrill","90, Rue Frédéric Chopin",27200,"VERNON","02.33.88.84.19","test","test",1);
-	/*$myToken = $test->connexion("test", "test");*/
-	/*echo $myToken;*/
