43bbfef797102ce473065ca52e7cfa48ba58dbad
olivier-maurie <olivier.maurie@gmail.com>
2015-04-30 09:15:10 +0200

    modifs test

--- model/test/testAdmin.php
+++ model/test/testAdmin.php
@@ -1,19 +1,19 @@
 <?php
-	require_once ("user.php");
-	require_once ("video.php");
+	require_once("../user.php");
+	require_once("../video.php");
+	
 	$test = new User(1,"VerrillDuplessis@jourrapide.com", "Duplessis", "Verrill","90, Rue Frédéric Chopin",27200,"VERNON","02.33.88.84.19","test","test",NULL);
 	/*$myToken = $test->connexion("test", "test");*/
 	/*echo $myToken;*/
 	
 	echo "<h2>user.php</h2>
 			<h3>Test afficheEmployee</h3>";
-	$afficheTableau = $test->displayEmployee("VideoStore" ,"VerrillDuplessis@jourrapide.com" ,'Duplessis' ,'Verrill' ,'90, Rue Frédéric Chopin' ,27200 ,'VERNON' ,'02.33.88.84.19');
-	var_dump($afficheTableau);
+	$afficheEmployee = new User(99 ,'VerrillDuplessis@jourrapide.com' ,'Duplessis' ,'Verrill' ,'90, Rue Frédéric Chopin' ,27200 ,'VERNON' , 'testEmployee', 'test', '02.33.88.84.19', 5, "VideoStore");
+	var_dump($afficheEmployee);
 
 	echo "<h3>Test afficheClient</h3>";
 	$testClient = new User(1,"admin@videostore.fr",	"Dupéré","Robert","27, Rue de Strasbourg",63100,"CLERMONT-FERRAND","04.06.05.23.12","admin", "admin", null);
-	$afficheTableauBis = $testClient->displayClient("VerrillDuplessis@jourrapide.com", "Duplessis", "Verrill", "90, Rue Frédéric Chopin", 27200, "VERNON", "02.33.88.84.19" );
-	var_dump($afficheTableauBis);
+	var_dump($testClient);
 	
 	echo "<h2>video.php</h2>
 			<h3>Test afficheVideo</h3>";
