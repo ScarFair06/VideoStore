d373cbf106c1f8cba7c82e740dec97a67a50725d
olivier-maurie <olivier.maurie@gmail.com>
2015-05-13 15:32:54 +0200

    delete index_admin.html

--- view/administration.php
+++ view/administration.php
@@ -1,8 +1,41 @@
 <?php session_start(); ?>
-<nav>
-	<a href="gestionvideo"><button>Gestion des vidéos</button></a>
-	<a href="gestionclient"><button>Gestion des clients</button></a>
-	<a href="gestionreservation"><button>Gestion des réservations</button></a>
-	<a href="gestionretard"><button>Gestion des retards</button></a>
-	<a href="deconnexion"><button>Déconnexion</button></a>
-</nav>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<title>VideoStore</title>
+
+	<!-- CSS  -->
+	<link href="lib/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="lib/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#!" class="brand-logo">VideoStore</a>
+			<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+			<ul class="right hide-on-med-and-down">
+				<li><a href="sass.html">Utilisateurs</a></li>
+				<li><a href="sass.html">RÃ©servations</a></li>
+				<li><a href="sass.html">Stock</a></li>
+				<li><a href="sass.html">Mon compte</a></li>
+				<li><a href="components.html">DÃ©connxion</a></li>
+			</ul>
+			<ul class="side-nav" id="mobile-demo">				
+				<li><a href="sass.html">Utilisateurs</a></li>
+				<li><a href="sass.html">RÃ©servations</a></li>
+				<li><a href="sass.html">Stock</a></li>
+				<li><a href="sass.html">Mon compte</a></li>
+				<li><a href="components.html">DÃ©connexion</a></li>
+			</ul>
+		</div>
+	</nav>
+
+
+	<!--  Scripts-->
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="lib/js/materialize.js"></script>
+	<script src="lib/js/init.js"></script>
+</body>
+</html>
---
+++ view/gestionclient.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8"/>
+<nav>
+	<a href="gestionvideo"><button>Gestion des vidéos</button></a>
+	<a href="gestionclient"><button>Gestion des clients</button></a>
+	<a href="gestionreservation"><button>Gestion des réservations</button></a>
+	<a href="gestionretard"><button>Gestion des retards</button></a>
+	<a href="deconnexion"><button>Déconnexion</button></a>
+</nav>
+<table>
+	<tr>
+		<td>Nom</td>
+		<td>Prénom</td>
+		<td>Téléphone</td>
+		<td>Mail</td>
+		<td>
+	</tr>
+</table>
